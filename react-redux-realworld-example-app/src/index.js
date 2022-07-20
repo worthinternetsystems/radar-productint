af873f201822636813873b25e6212af37d800152
domhall <dhall@worth.systems>
2022-07-20 14:43:39 +0100

    Added a failing button and sentry.io

--- react-redux-realworld-example-app/src/index.js
+++ react-redux-realworld-example-app/src/index.js
@@ -1,20 +1,32 @@
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import React from 'react';
-import { store, history} from './store';
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import React from "react";
+import { store, history } from "./store";
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
 
-import { Route, Switch } from 'react-router-dom';
-import { ConnectedRouter } from 'react-router-redux';
+import { Route, Switch } from "react-router-dom";
+import { ConnectedRouter } from "react-router-redux";
 
-import App from './components/App';
+import App from "./components/App";
+Sentry.init({
+  dsn: "https://65f6643fadb448c38a698f1fe62331e0@o1326996.ingest.sentry.io/6587593",
+  integrations: [new BrowserTracing()],
 
-ReactDOM.render((
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
+ReactDOM.render(
   <Provider store={store}>
     <ConnectedRouter history={history}>
       <Switch>
         <Route path="/" component={App} />
       </Switch>
     </ConnectedRouter>
-  </Provider>
+  </Provider>,
 
-), document.getElementById('root'));
+  document.getElementById("root")
+);
